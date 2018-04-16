9c4d5163b4c36ef31bd0bdfd385f6bc19ecb38fb
Daiane Bushey <daianebushey@gmail.com>
2018-04-16 05:54:58 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,10 @@ def my_select(collection)
   i = 0
   selection = []
   while i < collection.length
-    selection << yield(i)
-    i+=1
+    if collection[i]
+      selection << yield(i)
+      i+=1
+    end
   end
   selection
 end
